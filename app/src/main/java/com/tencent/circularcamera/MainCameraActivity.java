f42ae48a062db4dc7916b516780d4fadf08cf6c8
maxpengli <759581712@qq.com>
2017-03-08 17:20:19 +0800

    上传CircularCamera项目

---
+++ app/src/main/java/com/tencent/circularcamera/MainCameraActivity.java
@@ -0,0 +1,50 @@
+package com.tencent.circularcamera;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainCameraActivity extends AppCompatActivity {
+
+    CircularCameraSurfaceView glSurfaceView = null;
+    ImageButton shutterBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_camera);
+        glSurfaceView = (CircularCameraSurfaceView)findViewById(R.id.camera_textureview);
+        shutterBtn = (ImageButton)findViewById(R.id.btn_shutter);
+        shutterBtn.setOnClickListener(new BtnListeners());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+
+
+    private class BtnListeners implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.btn_shutter:
+                    CameraInterface.getInstance().doTakePicture();
+                    break;
+                default:break;
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/tencent/circularcamera/PermissionActivity.java
@@ -0,0 +1,70 @@
+package com.tencent.circularcamera;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by maxpengli on 2017/3/8.
+ */
+public class PermissionActivity extends AppCompatActivity {
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
+    private static final int REQUEST_CODE = 0; // 请求码
+
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.CAMERA
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPermissionsChecker = new PermissionsChecker(this);
+        Log.i("xp", "PermissionActivity oncreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("xp", "PermissionActivity onResume");
+        // 缺少权限时, 进入权限配置页面
+        if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+            Log.i("xp", "request permission");
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_CODE);//自定义的code
+        }
+        else{
+            startMainActivity();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.i("xp", "onRequestPermissionResult");
+        //可在此继续其他操作。
+        if (requestCode == REQUEST_CODE)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){//授权成功
+                Log.i("xp", "get permission success");
+                startMainActivity();
+            }
+            else {
+                Log.i("xp", "get permission failed");
+                finish();
+            }
+        }
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent();
+        intent.setClass(PermissionActivity.this, MainCameraActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
