ac737b9552240431e1d58bdf41cf561b7194f24a
maxpengli <759581712@qq.com>
2017-03-10 10:54:21 +0800

    添加触摸移动操作的监听

--- app/src/main/java/com/tencent/circularcamera/CircularCamerManager.java
+++ app/src/main/java/com/tencent/circularcamera/CircularCamerManager.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
@@ -65,6 +67,7 @@ public class CircularCamerManager {
 
 
     private void init(Context context){
+        this.context = context;
         if (isshow){
             return;
         }
@@ -81,6 +84,8 @@ public class CircularCamerManager {
     }
 
 
+
+
     private void initWindow(Context context) {
         mWindowManager = (WindowManager)context.getApplicationContext().getSystemService(context.WINDOW_SERVICE);
         wmParams = new WindowManager.LayoutParams();
@@ -92,19 +97,52 @@ public class CircularCamerManager {
         //设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
         wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
         //调整悬浮窗显示的停靠位置为左侧置顶
-        wmParams.gravity = Gravity.CENTER;
+        wmParams.gravity = Gravity.TOP|Gravity.LEFT;
         // 以屏幕左上角为原点，设置x、y初始值，相对于gravity
-        wmParams.x = 0;
-        wmParams.y = 0;
+        wmParams.x = 800;
+        wmParams.y = 600;
         //设置悬浮窗口长宽数据
         wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
         wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
         LayoutInflater inflater = LayoutInflater.from(context);
         //获取浮动窗口视图所在布局
         mFloatLayout = (RelativeLayout) inflater.inflate(R.layout.activity_main_camera, null);
-
-        glSurfaceView =(CircularCameraSurfaceView) mFloatLayout.findViewById(R.id.camera_textureview);
-        //添加mFloatLayout
+        glSurfaceView = (CircularCameraSurfaceView) mFloatLayout.findViewById(R.id.camera_textureview);
+        mFloatLayout.setOnTouchListener(new View.OnTouchListener() {
+            private float mTouchStartX;
+            private float mTouchStartY;
+            private float x;
+            private float y;
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                //获取相对屏幕的坐标，即以屏幕左上角为原点
+                x = event.getRawX();
+                y = event.getRawY() - 25;   //25是系统状态栏的高度
+//                Log.i("currP", "currX"+x+"====currY"+y);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:    //捕获手指触摸按下动作
+                        //获取相对View的坐标，即以此View左上角为原点
+                        mTouchStartX = event.getX();
+                        mTouchStartY = event.getY();
+//                        Log.i("startP", "startX" + mTouchStartX + "====startY" + mTouchStartY);
+                        break;
+                    case MotionEvent.ACTION_MOVE:   //捕获手指触摸移动动作
+                        //更新浮动窗口位置参数
+                        wmParams.x = (int) (x - mTouchStartX);
+                        wmParams.y = (int) (y - mTouchStartY);
+                        mWindowManager.updateViewLayout(view, wmParams);  //刷新显示
+                        break;
+                    case MotionEvent.ACTION_UP:    //捕获手指触摸离开动作
+                        //更新浮动窗口位置参数
+                        wmParams.x = (int) (x - mTouchStartX);
+                        wmParams.y = (int) (y - mTouchStartY);
+                        mWindowManager.updateViewLayout(view, wmParams);  //刷新显示
+                        mTouchStartX = mTouchStartY = 0;
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private void show(){
